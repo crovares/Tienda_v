50ccfdb50be13f6cf88d4bea9112fa302c246aa6
crova <crova@Carlo>
2023-08-07 16:27:34 -0600

    Semana 9

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -2,22 +2,27 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
-public class Categoria implements Serializable{
-    
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private long idCategoria;
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+
+    @OneToMany​
+    @JoinColumn(name = "id_categoria")
+    private List<Producto> productos; 
 
     public Categoria() {
     }
@@ -27,6 +32,4 @@ public class Categoria implements Serializable{
         this.activo = activo;
     }
 
-  
-    
 }
--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -3,24 +3,30 @@ package com.tienda.service;
 import com.tienda.domain.Producto;
 import java.util.List;
 
-
 public interface ProductoService {
-    
+
     //Se declara un metodo para obtener el listado y ArrayList de Objetos Producto
     //Los objetos vienen de la tabla producto
     //Son todos los registros o solo los activos
     public List<Producto> getProductos(boolean activos);
-    
+
     //Abajo se colocaran los metodos para un CRUD de productoas... pero en semana 6
-    
     // Se obtiene un Producto, a partir del id de un producto
     public Producto getProducto(Producto producto);
-    
+
     // Se inserta un nuevo producto si el id del producto esta vacío
     // Se actualiza un producto si el id del producto NO esta vacío
     public void save(Producto producto);
-    
+
     // Se elimina el producto que tiene el id pasado por parámetro
     public void delete(Producto producto);
+
+    // Lista de productos con precio entre x y y ordendados por descripción ConsultaAmpliada
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
     
+    //Lista de productos utilizando consultas con SQL Nativo
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
